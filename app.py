12ac34f276c12db4c72d3b6e54811dc477e5cfa9
Birkir Brynjarsson <birkir@birkir.is>
2018-12-05 13:30:09 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+import argparse
+
+def parseargs():
+    parser = argparse.ArgumentParser(
+        prog='AGR Database Deployment',
+        description='Database deployment script for AGR db development'
+    )
+    parser.add_argument('configfile', type=argparse.FileType('r'), help='config file for deployment with SqlPackage')
+    return parser.parse_args()
+
+
+# Checks if SqlPackage is installed and returns its path
+def getSqlPackage():
+    programFiles = os.getenv('ProgramFiles')
+    sqlpackage = '{}\\Microsoft SQL Server\\150\\DAC\\bin\\SqlPackage.exe'.format(programFiles)
+    if not os.path.isfile(sqlpackage):
+        print('SqlPackage.exe not found at: {}'.format(sqlpackage))
+        quit()
+    return sqlpackage
+
+
+def deployDB(config):
+    sqlPackage = getSqlPackage()        
+    print(sqlPackage)
+    subprocess.run(['ls', '-al'])
+    # for line in config:
+    #     print(line.strip())
+
+
+
+if __name__ == '__main__':
+    args = parseargs()
+    deployDB(args.configfile.readlines())
